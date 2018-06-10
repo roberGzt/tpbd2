6e4f9fa59ce317447391dc9da5adaf0dd2b78466
robergzt <rober.gerszen@gmail.com>
2018-06-10 19:44:34 -0300

    commit

--- tpAnterior/php/controlador/changePasswordController.php
+++ tpAnterior/php/controlador/changePasswordController.php
@@ -6,36 +6,20 @@ require_once ('../util/Session.php');
 
 $newURL = '../../changePassword.php';
 
-$passwordActual = filter_input(INPUT_POST, "passwordActual");
 $passwordNueva = filter_input(INPUT_POST, "passwordNueva");
-$passwordConfirmada = filter_input(INPUT_POST, "passwordConfirmar");
+$passwordConfirmada = filter_input(INPUT_POST, "passwordConfirmacion");
 
 if($passwordNueva != $passwordConfirmada)
 {
-    $newURL.= '?error=Las claves nuevas no coinciden';
+    $newURL.= '?error=Las claves son distintas';
 }
 else
 {
-    if($passwordActual == $passwordNueva)
-    {
-        $newURL.= '?error=La clave actual no puede ser igual a la nueva';
-    }
-    else
-    {
-        $persona = new Persona(getUserName(),null,null,$passwordActual);
-        $personaDao = new PersonaDao();
-        if(!$personaDao->login($persona))
-        {
-            $newURL.= '?error=La clave actual es erronea';
-        }
-        else
-        {
-            $persona->setClave($passwordNueva);
-            $personaDao->cambiarContraseña($persona);
-            $newURL.= '?success=Clave cambiada con exito';
-        }
-    }
-    
+    $persona = new Persona(getUserName(),null,null,$passwordActual);
+    $personaDao = new PersonaDao();
+    $persona->setClave($passwordNueva);
+    $personaDao->cambiarContraseña($persona);
+    $newURL.= '?success=Clave cambiada con exito';
 }
 header('Location: '.$newURL);
 
