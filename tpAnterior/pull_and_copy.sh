c61606cde45d1ccd04a3905ee9d09a01d2b143d9
roberGzt <rober.gerszen@gmail.com>
2018-06-10 11:12:34 -0300

    modificado script de pull para que haga clean de server directory

--- tpAnterior/pull_and_copy.sh
+++ tpAnterior/pull_and_copy.sh
@@ -1,1 +1,1 @@
-git pull && cp -r /home/demo/tpbd2/tpAnterior/* /home/demo/home && echo "done"
+git pull && rm -rf /home/demo/home/* && cp -r /home/demo/tpbd2/tpAnterior/* /home/demo/home && echo "done"
