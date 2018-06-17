e897cac5376680b2a9031bd9fb9f63834e407515
robergzt <rober.gerszen@gmail.com>
2018-06-17 13:06:12 -0300

    commit

--- php/controlador/listarPelisQueVieronController.php
+++ php/controlador/listarPelisQueVieronController.php
@@ -7,7 +7,7 @@
             $tuplas = $pelisQueVioDao->listarVistas();
             foreach($tuplas as $tupla)
             {
-                $html.= "<tr>";
+                $html.= "<tr class="text-center">";
                 $html.= "<td>" . $tupla->getUsuario1() . "</td>";
                 $html.= "<td>" . $tupla->getUsuario2() . "</td>";
                 $html.= "</tr>";
