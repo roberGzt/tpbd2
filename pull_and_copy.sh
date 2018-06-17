6afa80ff1f3edd28b8400490441203299bce86f5
robergzt <rober.gerszen@gmail.com>
2018-06-17 12:00:32 -0300

    reestructura del proyecto

---
+++ pull_and_copy.sh
@@ -0,0 +1,1 @@
+git pull && rm -rf /home/demo/home/* && cp -r /home/demo/tpbd2/* /home/demo/home && echo "Despliegue terminado."
--- tpAnterior/phpinfo.php
+++
@@ -1,6 +0,0 @@
-<?php
-phpinfo();
-echo getenv('INFORMIXSERVER');
-echo getenv('INFORMIXDIR');
-echo getenv('REMOTE_ADDR');
-?>
